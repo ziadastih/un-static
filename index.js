b489825ccae4fce78afb54672344e039374191bb
ziad astih <ziadastih12@outlook.com>
2022-11-25 17:44:45 +0200

    added the api

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ formBtn.forEach((btn) => {
       navigator.geolocation.getCurrentPosition(async (position) => {
         const { longitude, latitude } = position.coords;
         const coords = await axios.post(
-          "https://unhcr-lebanon.herokuapp.com/api/v1/coords",
+          "https://unhcr-api.onrender.com/api/v1/coords",
           {
             longitude: longitude,
             latitude: latitude,

